a18d0933de21f012802f1b16bdab5158003ec20f
PEDRO HENRIQUE PEREIRA DE QUEIROZ <101682986+PedroHP-queiroz@users.noreply.github.com>
2023-03-10 07:50:59 -0300

    update

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,35 @@
-import { BsSearch } from "react-icons/bs"
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>
+}
+import { useState, KeyboardEvent } from "react";
 
-const Search = () => {
+import { BsSearch } from "react-icons/bs";
+
+import classes from "./Search.module.css";
+
+const Search = ({loadUser}: SearchProps) => {
+  const [userName, setUserName] = useState("");
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if(e.key === "Enter") {
+      loadUser(userName)
+    }
+  }
+  
   return (
-    <div>
+    <div className={classes.search}>
         <h2>Busque por um usuário:</h2>
         <p>Conheça seus melhores repositórios.</p>
-        <div>
-            <input type="text" placeholder="Digite o nome do usuário" />
-            <button>
-                <BsSearch />
+        <div className={classes.search_container}>
+        <input type="text" placeholder="Digite o nome do usuário" onChange={(e) => setUserName(e.target.value)} 
+        onKeyDown = {handleKeyDown}
+        />
+            
+            <button onClick={() => loadUser(userName)}>
+              <BsSearch />
             </button>
         </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
